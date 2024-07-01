245c0480d8a31b6cbbdb089b4fa709b09100a15d
programmer-88 <ianmbayyz@gmail.com>
2024-07-01 09:59:51 +0300

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-  # Puppet, create a file in /tmp.
+# A puppet manifest to create a fie in /tmp directory
 
-file { '/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+file { 'school':
+ensure  => file,
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet',
+path    => '/tmp/school'
 }

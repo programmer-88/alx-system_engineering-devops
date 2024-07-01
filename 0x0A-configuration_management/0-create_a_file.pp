e3dbdce569f206476c694061fb9ceb805badf3e9
programmer-88 <ianmbayyz@gmail.com>
2024-07-01 09:56:30 +0300

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Puppet, create a file in /tmp.
+  # Puppet, create a file in /tmp.
 
-file { '/tmp/school':
-    ensure  => 'file',
-    content => 'I love Puppet',
-    mode    => '0744',
-    owner   => 'www-data',
-    group   => 'www-data',
-  }
+file { '/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
